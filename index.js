c0c9aed3e9055dc332290db9a70f1937c64fa653
Ayon <ayonjodder177@gmail.com>
2022-05-21 21:30:26 +0600

    JWT done

--- index.js
+++ index.js
@@ -14,6 +14,29 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+//JWT------------------------>
+function verifyJWT(req, res, next) {
+    const hederAuth = req.headers.authorization
+    if (!hederAuth) {
+        return res.status(401).send({ message: 'unauthorized access.try again' })
+    }
+    else {
+        const token = hederAuth.split(' ')[1]
+        console.log({ token });
+        jwt.verify(token, process.env.TOKEN, (err, decoded) => {
+
+            if (err) {
+                console.log(err);
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+            console.log('decoded', decoded);
+            req.decoded = decoded;
+            next()
+        })
+    }
+    console.log(hederAuth, 'inside chckjwt');
+
+}
 
 
 run().catch(console.dir)
