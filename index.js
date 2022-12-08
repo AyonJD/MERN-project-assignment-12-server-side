8cfbcc82c4ab22f47e307b5d539764f8b72939b5
Ayon Jodder <ayonjodder177@gmail.com>
2022-12-08 15:44:02 +0600

    Up

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const bodyParser = require("body-parser");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-console.log(process.env.STRIPE_SECRET_KEY);
 
 
 const port = process.env.PORT || 5000;
